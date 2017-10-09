5b2ca980124d22ddae2baae115921d3bf310cbc4
Navee Hans <nhans@bebotechnologies.com>
2017-10-09 12:25:41 +0530

    Commit12

---
+++ JenkinsDemoTestSolution/JenkinsDemoTestSolution/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nhans
+ * Date: 09-10-2017
+ * Time: 11:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace JenkinsDemoTestSolution
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# hello-world
-Practice Project for learning different languages and new technologies.
-I have made some changes to this file.
